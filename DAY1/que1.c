354db0027b15013e1c16ab57234e9eeb2f8b7a53
AYAN HALDER <ayanhalder242@gmail.com>
2023-06-21 12:43:12 +0530

    Update que1.c

--- DAY1/que1.c
+++ DAY1/que1.c
@@ -6,7 +6,8 @@ int find_biggest_if_else(int num1, int num2) {
     }
 }
 
-// Using ternary operator
 int find_biggest_ternary_operator(int num1, int num2) {
-    return (num1 > num2) ? num1 : num2;
+    
+    return (num1 > num2) ? num1 : num2;                  //// Using ternary operator
+
 }
